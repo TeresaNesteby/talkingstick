1bd7971128a8f3f39138d44ccb89f8a95ffac809
teresanesteby <teresa.nesteby@gmail.com>
2015-10-24 16:10:53 -0700

    nest routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
 
   resources :users do
-    resources :students
-    resources :periods
-    resources :notes
+    resources :periods do
+      resources :students
+        resources :notes
+
+    end
   end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
