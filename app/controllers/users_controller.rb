5cbf5104f1252bc828020ddb9ee90b7d22fc1ec8
teresanesteby <teresa.nesteby@gmail.com>
2015-10-31 13:54:08 -0700

    add pictures for background and fix links to login and signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,7 +13,7 @@ def show
     @user = User.new(user_params)
     if @user.save
       log_in @user
-      flash[:success] = "Welcome to the Sample App!"
+      flash[:success] = "Welcome to TalkingStick!"
       redirect_to @user
     else
       render 'new'
