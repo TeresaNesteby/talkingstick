0098edb85b3f463043e55b53c8ae47fde995e4be
teresanesteby <teresa.nesteby@gmail.com>
2015-11-02 22:50:02 -0800

    update landing page by deleting unnecessary information

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -5,14 +5,14 @@ module SessionsHelper
     session[:user_id] = user.id
   end
 
-    # Remembers a user in a persistent session.
+  # Remembers a user in a persistent session.
   def remember(user)
     user.remember
     cookies.permanent.signed[:user_id] = user.id
     cookies.permanent[:remember_token] = user.remember_token
   end
 
-   # Returns the current logged-in user (if any).
+  # Returns the current logged-in user (if any).
   def current_user
       if (user_id = session[:user_id])
       @current_user ||= User.find_by(id: user_id)
@@ -31,11 +31,11 @@ module SessionsHelper
     !current_user.nil?
   end
 
-  def forget(user)
-    user.forget
-    cookies.delete(:user_id)
-    cookies.delete(:remember_token)
-  end
+  # def forget(user)
+  #   user.forget
+  #   cookies.delete(:user_id)
+  #   cookies.delete(:remember_token)
+  # end
 
   # Logs out the current user.
   def log_out
